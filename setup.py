f540589d4ec5dac3edd7eb6aff86488b0ee4acee
Lyonk71 <lyonk71@gmail.com>
2018-12-09 21:44:35 -0500

    Enable README.md to be read by pypi via setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(name='pandas_dedupe',
-      version='0.2',
+      version='0.21',
       description='The Dedupe library made easy with Pandas.',
-      # url='http://github.com/storborg/funniest',
+      url='https://github.com/Lyonk71/pandas-dedupe',
       author='Keith Lyons',
       author_email='lyonk71@gmail.com',
       license='MIT',
@@ -13,4 +20,8 @@ setup(name='pandas_dedupe',
           'unidecode',
           'pandas',
       ],
-      zip_safe=False)
+      zip_safe=False,
+      
+      #Enable pypi description
+      long_description=long_description,
+      long_description_content_type='text/markdown')
